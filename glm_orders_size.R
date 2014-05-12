319c240df73133e9e41653b8c646534fa6707c89
mclapham <mclapham@ucsc.edu>
2014-05-12 15:37:08 -0700

    Create glm_orders_size.R

---
+++ glm_orders_size.R
@@ -0,0 +1,66 @@
+#Run data_acq.R first1
+#Logistic regression for six suprafamilial groups, using size as the predictor
+#code to plot fig. 5
+
+#####Logistic regression using size as the only predictor##### 
+odon_size_glm<-glm(type_part_binom~log_length, family=binomial,data=odon_occs) #odonatoptera
+
+col_size_glm<-glm(type_part_binom~log_length, family=binomial,data=col_occs) #coleoptera
+
+blatt_size_glm<-glm(type_part_binom~log_length, family=binomial,data=blatt_occs) #blattodea
+
+orth_size_glm<-glm(type_part_binom~log_length, family=binomial,data=orth_occs) #orthoptera
+
+hem_size_glm<-glm(type_part_binom~log_length, family=binomial,data=hem_occs) #hemiptera
+
+dip_size_glm<-glm(type_part_binom~log_length, family=binomial,data=dip_occs) #diptera
+
+#####PLOTS#####
+#dummy variable for prediction
+size.prd <- seq(-0.5,2.5, 0.05)
+
+pdf("fig5.pdf",width=8)
+
+par(mfrow = c(2, 3))
+par(mar=c(3,2.8,4,0))
+par(oma=c(0,0,0,1))
+
+#Plots Odonatoptera with logistic regression fits
+plot(odon_occs$log_length,jitter(odon_occs$type_part_binom,.2),yaxt="n", ylab='', xlab='',main="Odonatoptera",col='dark grey')
+axis(2, at=c(0,1),label=c("Non-Articulated","Articulated"), tick=T)
+lines(size.prd, predict.glm(odon_size_glm, data.frame(log_length=size.prd),type = "response"))
+mtext("A",adj=0)
+
+#Plots Blattodea with logistic regression fits
+plot(blatt_occs$log_length,jitter(blatt_occs$type_part_binom,.2), yaxt="n", ylab='',xlab='', main="Blattodea",col='dark grey')
+axis(2, at=c(0,1),label=F, tick=T)
+lines(size.prd, predict.glm(blatt_size_glm, data.frame(log_length=size.prd),type = "response"))
+mtext("B",adj=0)
+
+#Plots Orthoptera with logistic regression fits
+plot(orth_occs$log_length,jitter(orth_occs$type_part_binom,.2), yaxt="n", ylab='',xlab="", main="Orthoptera",col='dark grey')
+axis(2, at=c(0,1),label=F, tick=T)
+lines(size.prd, predict.glm(orth_size_glm, data.frame(log_length=size.prd),type = "response"))
+mtext("C",adj=0)
+
+par(mar=c(5,2.8,2,0))
+
+#Plots Hemiptera with logistic regression fits
+plot(hem_occs$log_length,jitter(hem_occs$type_part_binom,.2), yaxt="n", ylab='',xlab=expression("Wing element length (log"[10]*" mm)"),main="Hemiptera",col='dark grey')
+axis(2, at=c(0,1),label=c("Non-Articulated","Articulated"), tick=T)
+lines(size.prd, predict.glm(hem_size_glm, data.frame(log_length=size.prd),type = "response"))
+mtext("D",adj=0)
+
+#Plots Diptera with logistic regression fits
+plot(dip_occs$log_length,jitter(dip_occs$type_part_binom,.2), yaxt="n", ylab='',xlab=expression("Wing element length (log"[10]*" mm)"), main="Diptera",col='dark grey')
+axis(2, at=c(0,1),label=F, tick=T)
+lines(size.prd, predict.glm(dip_size_glm, data.frame(log_length=size.prd),type = "response"))
+mtext("E",adj=0)
+
+#Plots Coleoptera with logistic regression fits
+plot(col_occs$log_length,jitter(col_occs$type_part_binom,.2),yaxt="n", ylab='',xlab=expression("Wing element length (log"[10]*" mm)"), main="Coleoptera",col='dark grey')
+axis(2, at=c(0,1),label=F, tick=T)
+lines(size.prd, predict.glm(col_size_glm, data.frame(log_length=size.prd),type = "response"))
+mtext("F",adj=0)
+
+dev.off()
